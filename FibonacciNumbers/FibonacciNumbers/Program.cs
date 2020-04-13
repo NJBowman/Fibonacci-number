3deb1ffe94540b4e1399bb22dd14c535e68200af
Nick Bowman <42209122+NJBowman@users.noreply.github.com>
2020-04-13 13:12:18 +0100

    basic setup of solution

--- FibonacciNumbers/FibonacciNumbers/Program.cs
+++ FibonacciNumbers/FibonacciNumbers/Program.cs
@@ -6,7 +6,31 @@ namespace FibonacciNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(fibonacci());
         }
+
+        static long fibonacci()
+        {
+            //Step 1 - Base values
+            long max = 2000000;
+            long x = 1;
+            long y = 2;
+            long z = 0;
+
+            // Step 2 - add 2 terms to make a third
+            z = x + y;
+
+            // Step 3 -  transfer values to move onto further values
+            x = y;
+            y = z;
+
+            // Step 4 - repeat
+            // while(z < max) - prevent going above 4 million 
+            // { }
+
+
+            return z;
+        }
+
     }
 }
